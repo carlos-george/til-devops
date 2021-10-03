460444637d58d0963ae398f9a45c49db432bf475
CarlosGeorge <carlosgeorge248@gmail.com>
2021-10-03 07:54:18 -0300

    Project initialization

---
+++ til-todo-tasks/src/main/java/br/com/til/todotasks/TilTodoTasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.til.todotasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilTodoTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilTodoTasksApplication.class, args);
+	}
+
+}
---
+++ til-todo-tasks/src/test/java/br/com/til/todotasks/TilTodoTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.til.todotasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilTodoTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
